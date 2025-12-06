7bcde471e5329eeb12145f3310260a35e70662d9
bank-bonus <rkatov13@vk.com>
2025-12-07 02:55:13 +0500

    Add files via upload

---
+++ data/cartoons.ts
@@ -0,0 +1,66 @@
+import { CartoonEntry } from "../types";
+
+// Убедитесь, что папка 'images' существует в корне проекта
+// и содержит файлы с указанными именами в формате .jpg
+export const CARTOON_DATABASE: CartoonEntry[] = [
+  { 
+    title: "Ну, погоди!", 
+    imageUrl: "images/nu_pogodi.jpg" 
+  },
+  { 
+    title: "Винни-Пух", 
+    imageUrl: "images/vinni_puh.jpg" 
+  },
+  { 
+    title: "Трое из Простоквашино", 
+    imageUrl: "images/prostokvashino.jpg" 
+  },
+  { 
+    title: "Бременские музыканты", 
+    imageUrl: "images/bremenskie.jpg" 
+  },
+  { 
+    title: "Ёжик в тумане", 
+    imageUrl: "images/ezhik.jpg" 
+  },
+  { 
+    title: "Малыш и Карлсон", 
+    imageUrl: "images/karlson.jpg" 
+  },
+  { 
+    title: "Жил-был пёс", 
+    imageUrl: "images/zhil_byl_pes.jpg" 
+  },
+  { 
+    title: "Тайна третьей планеты", 
+    imageUrl: "images/taina_3_planety.jpg" 
+  },
+  { 
+    title: "Летучий корабль", 
+    imageUrl: "images/letuchiy_korabl.jpg" 
+  },
+  { 
+    title: "Крокодил Гена", 
+    imageUrl: "images/krokodil_gena.jpg" 
+  },
+  { 
+    title: "Приключения кота Леопольда", 
+    imageUrl: "images/leopold.jpg" 
+  },
+  { 
+    title: "Возвращение блудного попугая", 
+    imageUrl: "images/kesha.jpg" 
+  },
+  { 
+    title: "Падал прошлогодний снег", 
+    imageUrl: "images/last_year_snow.jpg" 
+  },
+  { 
+    title: "Умка", 
+    imageUrl: "images/umka.jpg" 
+  },
+  { 
+    title: "Маугли", 
+    imageUrl: "images/maugli.jpg" 
+  }
+];
