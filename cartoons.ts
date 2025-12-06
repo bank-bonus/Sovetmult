538ae42e74d2df236af4188e464e75a38c9c7ef5
bank-bonus <rkatov13@vk.com>
2025-12-07 01:57:56 +0500

    Add files via upload

---
+++ cartoons.ts
@@ -0,0 +1,38 @@
+import { CartoonEntry } from "../types";
+
+// ВСТАВЬТЕ СЮДА СВОИ КАРТИНКИ
+// Replace these URLs with links to your own images.
+export const CARTOON_DATABASE: CartoonEntry[] = [
+  {
+    title: "Ну, погоди!",
+    imageUrl: "https://i.ytimg.com/vi/mN6CjCmgXw0/maxresdefault.jpg"
+  },
+  {
+    title: "Винни-Пух",
+    imageUrl: "https://cdni.rbth.com/rbthmedia/images/2021.09/original/613b632985600a400c0683a4.jpg"
+  },
+  {
+    title: "Трое из Простоквашино",
+    imageUrl: "https://mf.b37mrtl.ru/rbthmedia/images/2019.06/original/5d03831985600a18413a9686.jpg"
+  },
+  {
+    title: "Бременские музыканты",
+    imageUrl: "https://i.ytimg.com/vi/3qD6zI3yC2A/maxresdefault.jpg"
+  },
+  {
+    title: "Ёжик в тумане",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/e/e0/Hedgehog_in_the_Fog.png"
+  },
+  {
+    title: "Малыш и Карлсон",
+    imageUrl: "https://i.ytimg.com/vi/n5Xy2y_rCqE/maxresdefault.jpg"
+  },
+  {
+    title: "Жил-был пёс",
+    imageUrl: "https://cdni.rbth.com/rbthmedia/images/2020.08/original/5f3e30e785600a354c253d53.jpg"
+  },
+  {
+    title: "Тайна третьей планеты",
+    imageUrl: "https://i.ytimg.com/vi/2y3TD3W2hUA/maxresdefault.jpg"
+  }
+];
