59b29ff73c2cbed4a87ffce2b98e89f7af754284
bank-bonus <rkatov13@vk.com>
2025-12-08 00:37:35 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import vkBridge from '@vkontakte/vk-bridge'; // Импортируем библиотеку
 import App from './App';
+
+// Если у тебя был файл index.css, раскомментируй строку ниже:
+// import './index.css'; 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-// Init VK Bridge
-if (window.vkBridge) {
-  window.vkBridge.send('VKWebAppInit')
-    .then(() => console.log('VK Bridge Initialized'))
-    .catch((e: any) => console.error('VK Bridge Init Failed', e));
-}
+// Инициализация ВК
+vkBridge.send('VKWebAppInit')
+  .then(() => console.log('VK Bridge Initialized'))
+  .catch((e) => console.error('VK Bridge Init Failed', e));
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
